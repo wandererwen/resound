ffc78a37525fa38102abacc547defba60d15d30e
wandererwen <80301765+wandererwen@users.noreply.github.com>
2021-09-12 21:36:45 +0800

    add all files

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: "#E47792",
+        secondary: {
+          100: "#627993",
+          200: "#5A6F87",
+        },
+      },
+      fontFamily: {
+        main: ["Josefin Sans"],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
